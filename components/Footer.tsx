c6123b886bd0f28c7de2058403eaa0c6b26b4791
hrmyid <sudo@blearning.my.id>
2021-12-08 02:17:48 +0700

    first test

---
+++ components/Footer.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { FaTwitter, FaGithub, FaLinkedin, FaTwitch,  FaGoodreads, FaYoutube, FaInstagram, FaTelegram } from 'react-icons/fa'
+import { SiLetterboxd } from 'react-icons/si'
+import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
+import * as config from 'lib/config'
+
+import styles from './styles.module.css'
+
+import { SocialLinks } from './SocialLinks'
+
+// TODO: merge the data and icons from PageSocial with the social links in Footer
+
+export const Footer: React.FC<{
+  isDarkMode: boolean
+  toggleDarkMode: () => void
+}> = ({ isDarkMode, toggleDarkMode }) => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const toggleDarkModeCb = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>Copyright © 2021 {config.author}</div>
+
+      {hasMounted ? (
+        <div className={styles.settings}>
+          <a
+            className={styles.toggleDarkMode}
+            onClick={toggleDarkModeCb}
+            title='Tottle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        </div>
+      ) : null}
+
+      <div className={styles.social}>
+        <SocialLinks />
+      </div>
+    </footer>
+  )
+}
---
+++ site.config - Copy.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '693a04a6acd9483eb7cc825b3364b836',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Jarrod Kane',
+  domain: 'JarrodKane.com',
+  author: 'Jarrod Kane',
+
+  // open graph metadata (optional)
+  description: 'A blog site about Jarrod Kane a wannabe athelet and a frontend developer',
+  socialImageTitle: 'Jarrod Kane',
+  socialImageSubtitle: `It's your boy Jarrod!`,
+
+  // social usernames (optional)
+  twitter: 'grime_goblin',
+  github: 'JarrodKane',
+  linkedin: 'jarrodkane',
+  youtube: 'jarrodkane',
+  twitch: 'grimegoblin',
+  letterboxd: 'jarrodK',
+  goodreads: "75192974-jarrod-kane",
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: "JarrodKane/blog-coments",
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
